d6dda205d04949cdb05fb976f87eb2f7695911f4
Adam Romański <adam.romanski@netguru.pl>
2018-05-21 13:32:52 +0200

    With template

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const serverless = require('serverless-http');
 const Vue = require('vue');
 const server = require('express')();
-const renderer = require('vue-server-renderer').createRenderer();
+const renderer = require('vue-server-renderer').createRenderer({
+  template: require('fs').readFileSync('./index.template.html', 'utf-8')
+});
 
 server.get('*', (req, res) => {
   const app = new Vue({
@@ -12,18 +14,8 @@ server.get('*', (req, res) => {
   });
 
   renderer.renderToString(app, (err, html) => {
-    if (err) {
-      res.status(500).end('Internal Server Error')
-      return
-    }
-    res.end(`
-      <!DOCTYPE html>
-      <html lang="en">
-        <head><title>Hello</title></head>
-        <body>${html}</body>
-      </html>
-    `);
-  });
+    console.log(html) // will be the full page with app content injected.
+  })
 })
 
 module.exports.handler = serverless(server);
