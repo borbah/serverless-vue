6476e9485610289ec201771bfd7192945d1b890d
Adam Romański <adam.romanski@netguru.pl>
2018-06-07 08:05:48 +0200

    Working example

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-import serverless from 'serverless-http';
-import Vue from 'vue';
-import express from 'express';
-import path from 'path';
-import fs from 'fs'
-import * as VueServerRenderer from 'vue-server-renderer'
+const serverless = require('serverless-http');
+const Vue = require('vue');
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const VueServerRenderer = require('vue-server-renderer');
 
-const  server = express()
+const  server = express();
 
-//static files are under example.com/templates/filename
-server.use('/templates', express.static(path.join(__dirname, 'templates')))
-const indexTemplate =  fs.readFileSync('templates/index.template.html', 'utf-8')
-const otherTemplate =  fs.readFileSync('templates/other.template.html', 'utf-8')
+server.use('/templates', express.static(path.join(__dirname, 'templates')));
+const indexTemplate =  fs.readFileSync('templates/index.template.html', 'utf-8');
+const otherTemplate =  fs.readFileSync('templates/other.template.html', 'utf-8');
 
 server.get('*', (req, res) => {
 	const renderer = VueServerRenderer.createRenderer({
@@ -18,7 +17,8 @@ server.get('*', (req, res) => {
 	});
 	const app = new Vue({
 		data: {
-			url: req.url
+			url: req.url,
+			custom: 'Dupa',
 		},
 		template: otherTemplate });
 		renderer.renderToString(app, (err, html) => {
@@ -27,7 +27,6 @@ server.get('*', (req, res) => {
 			}
 			res.status(200).send(html);
 		})
-})
-
+});
 
 module.exports.handler = serverless(server);
